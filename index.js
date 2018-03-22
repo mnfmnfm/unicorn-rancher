e0a8bab84afb01d97dd2d2023e74f19cc9ea225d
mnfmnfm <mnfmnfm@gmail.com>
2018-03-22 12:27:43 -0700

    basic page is real

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
-  res.send("Hello World!");
+  res.render("layout");
 });
 
 const PORT = process.env.PORT || 3000;
