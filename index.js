c54ce1a0e00d09895ecc2a683e78799d01e2f484
mnfmnfm <mnfmnfm@gmail.com>
2018-03-22 12:19:48 -0700

    initial commit, hello world!

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}`);
+});
